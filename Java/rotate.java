966e5061d84b97078cdb3fa95eac8a033f8b0b35
Rinju Sherpa <rinju54@gmail.com>
2021-04-19 23:30:14 -0500

    rotate left -> array elemenents

--- Java/rotate.java
+++ Java/rotate.java
@@ -1,16 +1,16 @@
+/*
+The rotateLeft method takes an array of integers, and an integer number of places, and returns a new copy of the array 
+that contains a "rotated" version of the input array. Rotation means that elements have been shifted leftwards by that 
+number of places, moving elements off one end of the array and onto the other end.The five tests are:
 
-// The rotateLeft method takes an array of integers, and an integer number of places, and returns a new copy of the array 
-//that contains a "rotated" version of the input array. Rotation means that elements have been shifted leftwards by that 
-//number of places, moving elements off one end of the array and onto the other end.The five tests are:
+rotating [0, 1, 2, 3, 4] by 0 should give [0, 1, 2, 3, 4]
+rotating [11, 12, 13, 14, 15] by 1 should give [12, 13, 14, 15, 11]
+rotating [11, 12, 13, 14, 15] by 2 should give [13, 14, 15, 11, 12]
+rotating [11, 12, 13, 14, 15] by 6 should give [12, 13, 14, 15, 11]
+*/
 
-// rotating [0, 1, 2, 3, 4] by 0 should give [0, 1, 2, 3, 4]
-// rotating [11, 12, 13, 14, 15] by 1 should give [12, 13, 14, 15, 11]
-// rotating [11, 12, 13, 14, 15] by 2 should give [13, 14, 15, 11, 12]
-// rotating [11, 12, 13, 14, 15] by 6 should give [12, 13, 14, 15, 11]
 
 import java.util.Arrays;
-
-
 public class rotate {
 	public static void main(String[] args) {
 		int[] a = new int[] {1,2,3,4,5};  // [
